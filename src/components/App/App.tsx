f32bfe30bf71f3a246d3e23ee13d614eed462702
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-02-16 23:04:47 +0100

    fix/minor fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import { Header } from "../Header/Header";
+import { Main } from "../Main/Main";
 import React from "react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 };
